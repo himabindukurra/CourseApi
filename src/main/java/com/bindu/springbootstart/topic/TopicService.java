ecf6b7c943f7ac9318a6e8f2a147b3fdc9c8f1bd
Himabindu Kurra <kurrahimabindhu@gmail.com>
2019-01-23 16:13:42 -0500

    created POST requests for topic

--- src/main/java/com/bindu/springbootstart/topic/TopicService.java
+++ src/main/java/com/bindu/springbootstart/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.bindu.springbootstart.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	List<Topic> topics = Arrays.asList(
+	List<Topic> topics = new ArrayList<> (Arrays.asList(
 			new Topic("java","Core Java","fundamentals of core java"),
 			new Topic("javaee","Advanced Java","advanced concepts of java"),
 			new Topic("db","mysql","configuration of database")
-			);
+			));
 	
 	List<Topic> getAllTopics(){
 		
@@ -21,5 +22,9 @@ public class TopicService {
 	
 	Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
